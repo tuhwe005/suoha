f96d9c29ff85b84744cbd17ca6bddcc36e12d32e
tuhwe005 <96981681+tuhwe005@users.noreply.github.com>
2023-06-19 21:48:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ wget -q https://raw.githubusercontent.com/tuhwe005/suoha/main/censys.sh
 wget -q https://raw.githubusercontent.com/tuhwe005/suoha/main/ip.txt
 wget -q https://raw.githubusercontent.com/tuhwe005/suoha/main/iptestport
 
-# Run asscan.sh script
+# Run censys.sh script
 ./censys.sh
