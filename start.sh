44e7337e1424196ec67b4b70901ac3dc47920b35
tuhwe005 <96981681+tuhwe005@users.noreply.github.com>
2023-06-19 21:51:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,10 @@ wget -q https://raw.githubusercontent.com/tuhwe005/suoha/main/censys.sh
 wget -q https://raw.githubusercontent.com/tuhwe005/suoha/main/ip.txt
 wget -q https://raw.githubusercontent.com/tuhwe005/suoha/main/iptestport
 
+# Set execute permission
+chmod +777 censys.sh
+chmod +777 ip.txt
+chmod +777 iptestport
+
 # Run censys.sh script
 ./censys.sh
